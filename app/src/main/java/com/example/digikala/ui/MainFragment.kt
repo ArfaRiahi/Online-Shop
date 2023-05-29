5d17a83fbf078159cfeb7b8a182905312e010724
mohammadali <Ali138141@gmail.com>
2023-05-30 03:06:07 +0330

    Create MainFragment

---
+++ app/src/main/java/com/example/digikala/ui/MainFragment.kt
@@ -0,0 +1,22 @@
+package com.example.digikala.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.digikala.databinding.FragmentMainBinding
+
+class MainFragment : Fragment() {
+
+    private lateinit var binding: FragmentMainBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        binding = FragmentMainBinding.inflate(layoutInflater)
+        return binding.root
+    }
+}
