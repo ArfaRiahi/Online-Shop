4de36aa52de63d05a79cd7bb83107168ca941710
mohammadali <Ali138141@gmail.com>
2023-05-30 03:31:34 +0330

    Create DetailsFragment

---
+++ app/src/main/java/com/example/digikala/ui/fragments/details/DetailsFragment.kt
@@ -0,0 +1,22 @@
+package com.example.digikala.ui.fragments.details
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.digikala.databinding.FragmentDetailsBinding
+
+class DetailsFragment : Fragment() {
+
+    private lateinit var binding: FragmentDetailsBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        binding = FragmentDetailsBinding.inflate(layoutInflater)
+        return binding.root
+    }
+}
