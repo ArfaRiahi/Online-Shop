c2d7ee1435fb66de2dfcd728f0207df195780c65
mohammadali <Ali138141@gmail.com>
2023-06-03 14:17:59 +0330

    Create SliderAdapter.kt

---
+++ app/src/main/java/com/example/digikala/ui/fragments/main/SliderAdapter.kt
@@ -0,0 +1,34 @@
+package com.example.digikala.ui.fragments.main
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import com.bumptech.glide.Glide
+import com.example.digikala.R
+import com.smarteist.autoimageslider.SliderViewAdapter
+
+class SliderAdapter(private val imageUrl: ArrayList<String>) :
+    SliderViewAdapter<SliderAdapter.SliderViewHolder>() {
+
+    override fun getCount(): Int {
+        return imageUrl.size
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup?): SliderAdapter.SliderViewHolder {
+        val inflate: View =
+            LayoutInflater.from(parent!!.context).inflate(R.layout.slider_item, parent)
+        return SliderViewHolder(inflate)
+    }
+
+    override fun onBindViewHolder(viewHolder: SliderAdapter.SliderViewHolder?, position: Int) {
+        if (viewHolder != null) {
+            Glide.with(viewHolder.itemView).load(imageUrl[position]).fitCenter()
+                .into(viewHolder.imageView)
+        }
+    }
+
+    class SliderViewHolder(itemView: View?) : SliderViewAdapter.ViewHolder(itemView) {
+        var imageView: ImageView = itemView!!.findViewById(R.id.sliderImage)
+    }
+}
