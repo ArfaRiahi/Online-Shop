29244ec3a10ec0b7c265f14d87bc0854aa4e627c
mohammadali <Ali138141@gmail.com>
2023-06-07 01:47:23 +0330

    Create Repository

---
+++ app/src/main/java/com/example/digikala/data/repository/Repository.kt
@@ -0,0 +1,24 @@
+package com.example.digikala.data.repository
+
+import com.example.digikala.data.models.products.ProductsResponse
+import com.example.digikala.data.remote.ProductsApiService
+import javax.inject.Inject
+
+class Repository @Inject constructor(private val appService: ProductsApiService) {
+
+    suspend fun getIdItemsProducts(id: Int): ProductsResponse {
+        return appService.getProductsItemsId(id)
+    }
+
+    suspend fun getNewestProducts(page: Int, perPage: Int): ProductsResponse {
+        return appService.getNewestProducts(page, perPage)
+    }
+
+    suspend fun getMostVisitedProducts(page: Int, perPage: Int): ProductsResponse {
+        return appService.getMostVisitedProducts(page, perPage)
+    }
+
+    suspend fun getTopRatedProducts(page: Int, perPage: Int): ProductsResponse {
+        return appService.getTopRatedProducts(page, perPage)
+    }
+}
