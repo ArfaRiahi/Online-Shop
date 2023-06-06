eb28332ecae1fc70a97fdccbd5fcdaef3d52fb71
mohammadali <Ali138141@gmail.com>
2023-06-07 01:21:49 +0330

    Create Const and save network information

---
+++ app/src/main/java/com/example/digikala/util/Const.kt
@@ -0,0 +1,9 @@
+package com.example.digikala.util
+
+object Const {
+    const val API_KEY = "ck_e72cd5a7e9408f8aa5bab82dac8e0e6ad99d60e8"
+    const val API_SECRET = "cs_4d0ef20e81d0a858f1b8882a5b0693555cdb9782"
+    const val BASE_URL = "https://woocommerce.maktabsharif.ir/wp-json/wc/v3/"
+    const val CONSUMER_KEY = "consumer_key"
+    const val CONSUMER_SECRET = "consumer_secret"
+}
