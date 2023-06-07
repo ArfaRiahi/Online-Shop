db0c9788d2a093311805466422edea34ea569689
mohammadali <Ali138141@gmail.com>
2023-06-08 03:19:39 +0330

    set adapter and observers in MainFragment

--- app/src/main/java/com/example/digikala/ui/fragments/main/MainFragment.kt
+++ app/src/main/java/com/example/digikala/ui/fragments/main/MainFragment.kt
@@ -1,30 +1,43 @@
 package com.example.digikala.ui.fragments.main
 
+import android.content.ContentValues
 import android.os.Bundle
-import android.view.LayoutInflater
+import android.util.Log
 import android.view.View
-import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import com.example.digikala.R
 import com.example.digikala.databinding.FragmentMainBinding
 import com.smarteist.autoimageslider.SliderView
+import dagger.hilt.android.AndroidEntryPoint
 
-class MainFragment : Fragment() {
+@AndroidEntryPoint
+class MainFragment : Fragment(R.layout.fragment_main) {
 
     private lateinit var binding: FragmentMainBinding
-
     private lateinit var imageUrl: ArrayList<String>
     private lateinit var sliderView: SliderView
     private lateinit var sliderAdapter: SliderAdapter
+    private val viewModel: MainFragmentViewModel by viewModels()
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        // Inflate the layout for this fragment
-        binding = FragmentMainBinding.inflate(layoutInflater)
+    private lateinit var adapterNewest: MainRecyclersAdapter
+    private lateinit var adapterMostVisited: MainRecyclersAdapter
+    private lateinit var adapterTopRated: MainRecyclersAdapter
+    private lateinit var navController: NavController
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = DataBindingUtil.bind(view)!!
+        navController = Navigation.findNavController(view)
+        setUi()
         setUpSliderView()
-        return binding.root
+        observer()
     }
+
 
     private fun setUpSliderView() {
         sliderView = binding.slider
@@ -38,5 +51,48 @@ class MainFragment : Fragment() {
         sliderView.scrollTimeInSec = 2
         sliderView.isAutoCycle = true
         sliderView.startAutoCycle()
+    }
+
+    private fun setUi() {
+        createNewestAdapter()
+        createMostVisitedAdapter()
+        createTopRatedAdapter()
+    }
+
+    private fun createNewestAdapter() {
+        adapterNewest = MainRecyclersAdapter(onClick = {
+            navController.navigate(MainFragmentDirections.actionMainFragmentToDetailsFragment(it))
+        })
+        binding.recyclerNewest.adapter = adapterNewest
+    }
+
+    private fun createMostVisitedAdapter() {
+        adapterMostVisited = MainRecyclersAdapter(onClick = {
+            navController.navigate(MainFragmentDirections.actionMainFragmentToDetailsFragment(it))
+        })
+        binding.recyclerMostViewed.adapter = adapterMostVisited
+    }
+
+    private fun createTopRatedAdapter() {
+        adapterTopRated = MainRecyclersAdapter(onClick = {
+            navController.navigate(MainFragmentDirections.actionMainFragmentToDetailsFragment(it))
+        })
+        binding.recyclerTopRated.adapter = adapterTopRated
+    }
+
+    private fun observer() {
+        viewModel.newestProduct.observe(viewLifecycleOwner) {
+            adapterNewest.submitList(it)
+        }
+
+        viewModel.mostVisitedProduct.observe(viewLifecycleOwner) {
+            adapterMostVisited.submitList(it)
+            Log.e(ContentValues.TAG, "setUi: ${it[0]}")
+        }
+
+        viewModel.topRatedProduct.observe(viewLifecycleOwner) {
+            adapterTopRated.submitList(it)
+            Log.e(ContentValues.TAG, "setUi: ${it[0]}")
+        }
     }
 }
