0c638a05f2ee1e66d8a9715b41a1f59cf6252b79
mohammadali <Ali138141@gmail.com>
2023-06-08 13:41:01 +0330

    setup navController with bottom navigation

--- app/src/main/java/com/example/digikala/ui/MainActivity.kt
+++ app/src/main/java/com/example/digikala/ui/MainActivity.kt
@@ -3,48 +3,35 @@ package com.example.digikala.ui
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.WindowCompat
-import androidx.fragment.app.Fragment
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 import com.example.digikala.R
 import com.example.digikala.databinding.ActivityMainBinding
-import com.example.digikala.ui.fragments.category.CategoryFragment
-import com.example.digikala.ui.fragments.main.MainFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+    private lateinit var bottomNavigationView: BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         transparentStatusBar()
-        loadFragment(MainFragment())
-
-        //bottom navigation setup
-        binding.bottomNavigationView.setOnItemSelectedListener {
-            when (it.itemId) {
-                R.id.mainFragment -> {
-                    loadFragment(MainFragment())
-                    true
-                }
-
-                R.id.categoryFragment -> {
-                    loadFragment(CategoryFragment())
-                    true
-                }
-
-                else -> {
-                    false
-                }
-            }
-        }
+        setBottomNavigation()
     }
 
-    private fun loadFragment(fragment: Fragment) {
-        val transaction = supportFragmentManager.beginTransaction()
-        transaction.replace(binding.fragmentContainerView.id, fragment)
-        transaction.commit()
+    private fun setBottomNavigation() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        navController = navHostFragment.navController
+        bottomNavigationView = binding.bottomNavigationView
+        NavigationUI.setupWithNavController(bottomNavigationView, navController)
     }
 
     private fun transparentStatusBar() {
