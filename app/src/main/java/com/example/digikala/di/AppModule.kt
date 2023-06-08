344925d6b0822fe6ca2b4df2a1da432413bf689c
mohammadali <Ali138141@gmail.com>
2023-06-08 13:41:44 +0330

    increase interceptor timeOut

--- app/src/main/java/com/example/digikala/di/AppModule.kt
+++ app/src/main/java/com/example/digikala/di/AppModule.kt
@@ -15,6 +15,7 @@ import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
 
 @Module
@@ -57,6 +58,9 @@ object AppModule {
         return OkHttpClient.Builder()
             .addInterceptor(interceptor)
             .addInterceptor(httpLoggingInterceptor)
+            .connectTimeout(10,TimeUnit.SECONDS)
+            .writeTimeout(10,TimeUnit.SECONDS)
+            .readTimeout(30,TimeUnit.SECONDS)
             .build()
     }
 
