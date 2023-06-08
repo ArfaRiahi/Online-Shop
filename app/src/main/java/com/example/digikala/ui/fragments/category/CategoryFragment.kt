1a3c7823f8fc1f9f8f75de40a25353bbb93ae53f
mohammadali <Ali138141@gmail.com>
2023-06-09 01:03:07 +0330

    write adapter and init for CategoryFragment.kt

--- app/src/main/java/com/example/digikala/ui/fragments/category/CategoryFragment.kt
+++ app/src/main/java/com/example/digikala/ui/fragments/category/CategoryFragment.kt
@@ -1,22 +1,46 @@
 package com.example.digikala.ui.fragments.category
 
 import android.os.Bundle
-import android.view.LayoutInflater
+import android.util.Log
 import android.view.View
-import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
+import com.example.digikala.R
 import com.example.digikala.databinding.FragmentCategoryBinding
+import dagger.hilt.android.AndroidEntryPoint
 
-class CategoryFragment : Fragment() {
+@AndroidEntryPoint
+class CategoryFragment : Fragment(R.layout.fragment_category) {
 
+    private val viewModel: CategoryFragmentViewModel by viewModels()
+    private lateinit var navController: NavController
     private lateinit var binding: FragmentCategoryBinding
+    private lateinit var categoryAdapter: CategoryFragmentRecyclerAdapter
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        // Inflate the layout for this fragment
-        binding = FragmentCategoryBinding.inflate(layoutInflater)
-        return binding.root
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = DataBindingUtil.bind(view)!!
+        binding.lifecycleOwner = viewLifecycleOwner
+        navController = findNavController()
+        setUi()
+        observer()
+
+    }
+
+    private fun setUi() {
+        viewModel.getCategoryList()
+        categoryAdapter = CategoryFragmentRecyclerAdapter{
+
+        }
+        binding.recyclerCategory.adapter = categoryAdapter
+    }
+
+    private fun observer() {
+        viewModel.categoryList.observe(viewLifecycleOwner) {
+            categoryAdapter.submitList(it)
+        }
     }
 }
