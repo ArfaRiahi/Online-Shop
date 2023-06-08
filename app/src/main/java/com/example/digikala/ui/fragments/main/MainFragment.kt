98e2700ba4e2b3d3ea99406180533f94ad1600ab
mohammadali <Ali138141@gmail.com>
2023-06-09 00:59:20 +0330

    Cleaning up MainFragment

--- app/src/main/java/com/example/digikala/ui/fragments/main/MainFragment.kt
+++ app/src/main/java/com/example/digikala/ui/fragments/main/MainFragment.kt
@@ -1,8 +1,6 @@
 package com.example.digikala.ui.fragments.main
 
-import android.content.ContentValues
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
@@ -20,7 +18,7 @@ class MainFragment : Fragment(R.layout.fragment_main) {
     private lateinit var binding: FragmentMainBinding
     private lateinit var imageUrl: ArrayList<String>
     private lateinit var sliderView: SliderView
-    private lateinit var sliderAdapter: SliderAdapter
+    private lateinit var sliderAdapter: SliderAdapterDashboard
     private val viewModel: MainFragmentViewModel by viewModels()
     private lateinit var adapterNewest: MainRecyclersAdapter
     private lateinit var adapterMostVisited: MainRecyclersAdapter
@@ -44,7 +42,7 @@ class MainFragment : Fragment(R.layout.fragment_main) {
         imageUrl.add("https://woocommerce.maktabsharif.ir/wp-content/uploads/2020/01/113792128.jpg")
         imageUrl.add("https://woocommerce.maktabsharif.ir/wp-content/uploads/2020/01/301185.jpg")
         imageUrl.add("https://woocommerce.maktabsharif.ir/wp-content/uploads/2023/05/T_2_front-2.jpg")
-        sliderAdapter = SliderAdapter(imageUrl)
+        sliderAdapter = SliderAdapterDashboard(imageUrl)
         sliderView.autoCycleDirection = SliderView.LAYOUT_DIRECTION_LTR
         sliderView.setSliderAdapter(sliderAdapter)
         sliderView.scrollTimeInSec = 2
@@ -86,12 +84,10 @@ class MainFragment : Fragment(R.layout.fragment_main) {
 
         viewModel.mostVisitedProduct.observe(viewLifecycleOwner) {
             adapterMostVisited.submitList(it)
-            Log.e(ContentValues.TAG, "setUi: ${it[0]}")
         }
 
         viewModel.topRatedProduct.observe(viewLifecycleOwner) {
             adapterTopRated.submitList(it)
-            Log.e(ContentValues.TAG, "setUi: ${it[0]}")
         }
     }
 }
