786195b4f0193460f4453ed349ae20907fddc166
mohammadali <Ali138141@gmail.com>
2023-06-09 13:42:53 +0330

    create ProductsListFragment

--- app/src/main/java/com/example/digikala/ui/fragments/pruductsList/ProductsListFragment.kt
+++ app/src/main/java/com/example/digikala/ui/fragments/pruductsList/ProductsListFragment.kt
@@ -1,22 +1,50 @@
 package com.example.digikala.ui.fragments.pruductsList
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
+import com.example.digikala.R
 import com.example.digikala.databinding.FragmentProductsListBinding
+import dagger.hilt.android.AndroidEntryPoint
 
-class ProductsListFragment : Fragment() {
+@AndroidEntryPoint
+class ProductsListFragment : Fragment(R.layout.fragment_products_list) {
 
     private lateinit var binding: FragmentProductsListBinding
+    private val viewModel: ProductListFragmentViewModel by viewModels()
+    private val args: ProductsListFragmentArgs by navArgs()
+    private lateinit var navController: NavController
+    private lateinit var productsAdapter: ProductListFragmentRecyclerAdapter
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = DataBindingUtil.bind(view)!!
+        binding.lifecycleOwner = viewLifecycleOwner
+        navController = findNavController()
+        setUi()
+        observer()
+    }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        // Inflate the layout for this fragment
-        binding = FragmentProductsListBinding.inflate(layoutInflater)
-        return binding.root
+    private fun setUi() {
+        productsAdapter = ProductListFragmentRecyclerAdapter {
+            val id = productsAdapter.currentList[it].id
+            navController.navigate(
+                ProductsListFragmentDirections.actionProductsListFragmentToDetailsFragment(
+                    id
+                )
+            )
+        }
+        binding.productListRecycler.adapter = productsAdapter
+    }
+
+    private fun observer() {
+        viewModel.getCProductList(args.productId)
+        viewModel.productsList.observe(viewLifecycleOwner) {
+            productsAdapter.submitList(it)
+        }
     }
 }
