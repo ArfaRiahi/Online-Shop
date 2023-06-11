57914b36f9a327560eef7d68345b22c652a2590d
mohammadali <Ali138141@gmail.com>
2023-06-11 10:47:42 +0330

    change Repository returned type to response

--- app/src/main/java/com/example/digikala/data/repository/Repository.kt
+++ app/src/main/java/com/example/digikala/data/repository/Repository.kt
@@ -3,35 +3,36 @@ package com.example.digikala.data.repository
 import com.example.digikala.data.models.category.CategoryResponse
 import com.example.digikala.data.models.products.ProductsResponse
 import com.example.digikala.data.remote.ProductsApiService
+import retrofit2.Response
 import javax.inject.Inject
 
 class Repository @Inject constructor(private val appService: ProductsApiService) {
 
-    suspend fun getIdItemsProducts(id: Int): ProductsResponse {
+    suspend fun getIdItemsProducts(id: Int): Response<ProductsResponse> {
         return appService.getProductsItemsId(id)
     }
 
-    suspend fun getNewestProducts(page: Int, perPage: Int): ProductsResponse {
+    suspend fun getNewestProducts(page: Int, perPage: Int): Response<ProductsResponse> {
         return appService.getNewestProducts(page, perPage)
     }
 
-    suspend fun getMostVisitedProducts(page: Int, perPage: Int): ProductsResponse {
+    suspend fun getMostVisitedProducts(page: Int, perPage: Int): Response<ProductsResponse> {
         return appService.getMostVisitedProducts(page, perPage)
     }
 
-    suspend fun getTopRatedProducts(page: Int, perPage: Int): ProductsResponse {
+    suspend fun getTopRatedProducts(page: Int, perPage: Int): Response<ProductsResponse> {
         return appService.getTopRatedProducts(page, perPage)
     }
 
-    suspend fun getCategoryLists(): CategoryResponse {
+    suspend fun getCategoryLists(): Response<CategoryResponse> {
         return appService.getProductsCategories()
     }
 
-    suspend fun getProductsList(productId: Int): ProductsResponse {
+    suspend fun getProductsList(productId: Int): Response<ProductsResponse> {
         return appService.getProductsLists(productId)
     }
 
-    suspend fun getSliderProducts(): ProductsResponse {
+    suspend fun getSliderProducts(): Response<ProductsResponse> {
         return appService.getSliderProducts()
     }
 }
